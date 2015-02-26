d806f34db83224e5b3136f9122debab00b4656bd
samueg <samuelgong81@gmail.com>
2015-02-26 14:26:22 +0800

    P, TS, L and D
    New development relating to Padding, Tag Spacing, Location and
    Dimension.

--- TagArea.js
+++ TagArea.js
@@ -25,8 +25,15 @@
     };
 
     RC.form.TagArea = RC.extend(RC.form.Field, {
-        constructor: function() {
-            RC.form.TagArea.superclass.constructor.apply(this, arguments);
+        constructor: function(config) {
+            config = config || {};
+            RC.apply(config, {
+                fixedPadding: 10,
+                tagSpacing: 5,
+                tags: new RC.MixedCollection()
+            });
+
+            RC.form.TagArea.superclass.constructor.apply(this, [config]);
         },
         render: function() {
             var self = this,
@@ -40,7 +47,8 @@
 
             textarea = new Element('textarea');
             textarea.setStyles({
-                resize: 'none'
+                resize: 'none',
+                padding: pixels(self.fixedPadding)
             });
             textarea.inject(view);
             textarea.addEvent('newTagIsGoingToBeCreated', function(event) {
@@ -51,13 +59,66 @@
                     tagLocation;
 
                 tag.compile(view);
-                tagLocation = calculateTagLocation(self, tag);
+                tagLocation = self._calculateNewTagLocation();
                 tag.setLocation(tagLocation);
                 textarea.setProperty('value', '');
+
+                self.tags.add(tag);
             });
 
             return view;
-        }
+        },
+        getId: function() {
+            return this.id;
+        },
+        _getTextArea: function() {
+            var self = this,
+                view = self.getRenderedCanvas()
+                ;
+            if (view) {
+                return view.getElement();
+            }
+        },
+        _getContentDimension: function() {
+            var self = this,
+                result,
+                view = self.getRenderedCanvas(),
+                size,
+                width,
+                height
+                ;
+
+            if (view) {
+                size = view.getElement('textarea').getSize();
+                width = size.x - (self.fixedPadding * 2);
+                height = size.y - (self.fixedPadding * 2);
+                result = new Dimension(width, height);
+            }
+
+            return result;
+        },
+        _calculateAvailableWidth: function() {
+
+        },
+        _calculateNewTagLocation: function() {
+            var self = this,
+                lastTag,
+                location,
+                dimension,
+                result = new Location();
+                ;
+
+            if (self.tags.length == 0) {
+                result = result.offset(self.fixedPadding, self.fixedPadding);
+            } else {
+                lastTag = self.tags.last();
+                location = lastTag.getLocation();
+                dimension = lastTag.getDimension();
+                result = result.offset(location).offset(dimension.getWidth()).offset(self.tagSpacing);
+            }
+
+            return result;
+        }        
     });
 
     var Tag = RC.extend(RC.Element, {
@@ -94,6 +155,25 @@
 
             return view;
         },
+        getId: function() {
+            return this.id;
+        },
+        getLocation: function() {
+            var self = this,
+                result,
+                view = self.getRenderedCanvas(),
+                offsetParent,
+                position
+                ;
+
+            if (view) {
+                offsetParent = view.getOffsetParent();
+                position = view.getPosition(offsetParent);
+                result = new Location(position.x, position.y);
+            }
+
+            return result;
+        },
         setLocation: function(location) {
             var self = this,
                 view = self.getRenderedCanvas();
@@ -101,25 +181,71 @@
                 left: location.getX(),
                 top: location.getY()
             });
+        },
+        getDimension: function() {
+            var self = this,
+                result,
+                view = self.getRenderedCanvas(),
+                size
+                ;
+
+            if (view) {
+                size = view.getSize();
+                result = new Dimension(size.x, size.y);
+            }
+
+            return result;
         }
 
     });
 
     var Location = new Class({
         GetterSetter: ['x', 'y'],
-        initialize: function() {
-            this.setX(0);
-            this.setY(0);
+        initialize: function(x, y) {
+            this.setX(x || 0);
+            this.setY(y || 0);
+        },
+        offset: function(x, y) {
+            var location, 
+                dimension
+                ;
+
+            if (x instanceof Location) {
+                location = x;
+                x = location.getX();
+                y = location.getY();
+            } else if (x instanceof Dimension) {
+                dimension = x;
+                x = dimension.getWidth();
+                y = dimension.getHeight();
+            }
+            x = x || 0;
+            y = y || 0;
+
+            return new Location(this.getX() + x, this.getY() + y);
         }
     });
 
     var Dimension = new Class({
-        width: 0,
-        height: 0
+        GetterSetter: ['width', 'height'],
+        initialize: function(width, height) {
+            this.setWidth(width || 0);
+            this.setHeight(height || 0);
+        },
+        add: function(dimension) {
+            var result
+                ;
+
+            result = new Dimension(self.getWidth() + dimension.getWidth(), 
+                self.getHeight() + dimension.getHeight());
+
+            return result;
+        }
     });
 
-    function calculateTagLocation(tagArea, tag) {
-        return new Location();
+    function pixels(value) {
+        value = parseInt(value);
+        return isNaN(value) ? '0px' : (value + 'px');
     }
 
     function calculateTagDimension(tagArea, tag) {
