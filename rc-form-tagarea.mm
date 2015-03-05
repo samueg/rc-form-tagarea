facdd1c57cd009578d0226299ba350fa2f545479
samueg <samuelgong81@gmail.com>
2015-03-05 15:46:09 +0800

    Design tag deletion use cases

---
+++ rc-form-tagarea.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425539431960" ID="ID_608018396" MODIFIED="1425539520204" TEXT="TagArea">
+<node CREATED="1425539530517" ID="ID_530900388" MODIFIED="1425539537032" POSITION="right" TEXT="Tag Deletion">
+<node CREATED="1425539542833" ID="ID_1809700558" MODIFIED="1425539552247" TEXT="Use Cases">
+<node CREATED="1425539556147" ID="ID_1146700110" MODIFIED="1425539575256" TEXT="Delete the first tag."/>
+<node COLOR="#000000" CREATED="1425539564722" ID="ID_1807062541" MODIFIED="1425541520107" TEXT="Delete the last tag.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1425539588156" ID="ID_1174106855" MODIFIED="1425539630297" TEXT="Delete a tag with a predecessor.">
+<node CREATED="1425539887102" ID="ID_804254716" MODIFIED="1425539897092" TEXT="The predecssor is in the same row."/>
+<node CREATED="1425539906031" ID="ID_1565243135" MODIFIED="1425539913867" TEXT="The predecssor is in the up row."/>
+</node>
+<node CREATED="1425539609157" ID="ID_896265862" MODIFIED="1425541496883" TEXT="Delete a tag with a follower.">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1425539927536" ID="ID_686045223" MODIFIED="1425539935981" TEXT="The follower is in the same row."/>
+<node CREATED="1425539939072" ID="ID_550737342" MODIFIED="1425539952972" TEXT="The follower is in the down row."/>
+</node>
+<node CREATED="1425539652638" ID="ID_588768994" MODIFIED="1425539670564" TEXT="Delete a tag with both a predecessor and a follower.">
+<node CREATED="1425539975009" ID="ID_910162413" MODIFIED="1425540005159" TEXT="Both the predecessor and the follower are in the same row."/>
+<node CREATED="1425540026435" ID="ID_1162263893" MODIFIED="1425540068488" TEXT="The predecessor is in the same row and the follower is in the down row."/>
+<node CREATED="1425540053612" ID="ID_1994519047" MODIFIED="1425540080010" TEXT="The predecessor is in the up row and the follower is in the same row."/>
+<node CREATED="1425540115022" ID="ID_455360374" MODIFIED="1425540120956" TEXT="The predecessor is in the up row and the follower is in the down row."/>
+</node>
+<node CREATED="1425539844844" ID="ID_146192717" MODIFIED="1425539850570" TEXT="Delete the only tag."/>
+</node>
+</node>
+</node>
+</map>
